6b55fbae302d506edb60da6f23b8c56079bca205
HughTribe <229113637+HughTribe@users.noreply.github.com>
2026-05-10 16:12:52 +0100

    commit

--- HelloGL/HelloGL/HelloGL.cpp
+++ HelloGL/HelloGL/HelloGL.cpp
@@ -13,8 +13,8 @@ HelloGL::HelloGL(int argc, char* argv[])
 
 	for (int i = 0; i < 200; i++)
 	{
-		cube[i] = new Cube(((rand() % 400) / 10.0f) - 20.0f, ((rand() % 200) / 10.0f) - 10.0f, -	(rand() % 1000 / 10.0f));
-	}
+		cube[i] = new Cube(((rand() % 400) / 10.0f) - 20.0f, ((rand() % 200) / 10.0f) - 10.0f, -(rand() % 1000 / 10.0f));
+	}	
 
 	//camera->eye.x = 5.0f; camera->eye.y = 5.0f; camera->eye.z = -5.0f;
 	camera->eye.x = 0.0f; camera->eye.y = 0.0f; camera->eye.z = 1.0f;
